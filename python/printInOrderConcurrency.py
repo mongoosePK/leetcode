f3264e655819e09d6a72563af4ba29e6210ae1da
mongoosePK <30559692+mongoosePK@users.noreply.github.com>
2021-05-12 10:56:16 -0500

    add a leetcode concurrency solution

---
+++ python/printInOrderConcurrency.py
@@ -0,0 +1,41 @@
+# printInOrderConcurrency.py
+# author: william pulkownik
+'''
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+
+The same instance of Foo will be passed to three different threads. 
+Thread A will call first(), thread B will call second(), 
+and thread C will call third(). Design a mechanism and modify the 
+program to ensure that second() is executed after first(), 
+and third() is executed after second()
+'''
+
+from threading import Lock
+# we will import and use the lowest level synchronization primitive available
+# the lock
+
+class Foo:
+    def __init__(self):
+        self.locks = (Lock(), Lock())
+        self.locks[0].acquire()
+        self.locks[1].acquire()
+
+    def first(self, printFirst):
+        printFirst()
+        self.locks[0].release()
+
+    def second(self, printSecond):
+        with self.locks[0]:
+            printSecond()
+            self.locks[1].release()
+    
+    def third(self, printThird):
+        with self.locks[1]:
+            printThird()
+            
